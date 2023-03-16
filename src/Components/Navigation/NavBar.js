dee487dd35b6db4836aa1a42eea75828b5281d93
Nanduni <nandunih@infyinnos.com>
2023-03-16 20:44:34 +0530

    inital commit

---
+++ src/Components/Navigation/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div className="navBarBox">
+        <div className="navBarLeft">
+          <span className='logo'>fakebook</span>
+        </div>
+        <div className="navBarCenter">
+          <div className="searchBarBox">
+            
+          </div>
+        </div>
+        <div className="navBarRight"></div>
+    </div>
+  )
+}
