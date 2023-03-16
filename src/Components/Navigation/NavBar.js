7e18b4f2f3353255efbead1f12433190cc0defa7
Nanduni <nandunih@infyinnos.com>
2023-03-16 22:33:38 +0530

    nav bar done

--- src/Components/Navigation/NavBar.js
+++ src/Components/Navigation/NavBar.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ChatIcon from '@mui/icons-material/Chat';
+import SettingsIcon from '@mui/icons-material/Settings';
+import './navBar.css';
 
 export default function NavBar() {
   return (
@@ -6,12 +11,35 @@ export default function NavBar() {
         <div className="navBarLeft">
           <span className='logo'>fakebook</span>
         </div>
+
         <div className="navBarCenter">
           <div className="searchBarBox">
-            
+            <SearchIcon  className="searchBarIcon"/>
+            <input placeholder='Search for your friends...' className="searchInput" />
           </div>
         </div>
-        <div className="navBarRight"></div>
+
+        <div className="navBarRight">
+          <div className="navBarLinks">
+            <span className="navBarLink">Home</span>
+            <span className="navBarLink">Profile</span>
+          </div>
+          <div className="navBarIcon">
+            <AccountCircleIcon/>
+            <span className="iconTag">3</span>
+          </div>
+          <div className="navBarIcon">
+            <ChatIcon/>
+            <span className="iconTag">2</span>
+          </div>
+          <div className="navBarIcon">
+            <SettingsIcon/>
+            <span className="iconTag">5</span>
+          </div>
+          <div className="pic">
+            <img src="./images/pic1.jpeg" className="profilePicture" />
+          </div>
+        </div>
     </div>
   )
 }
